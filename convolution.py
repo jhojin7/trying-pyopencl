db5fa98fcb4415bd9a6f8333d5799808567a1c50
Hojin Jang <jhojin7@gmail.com>
2022-06-12 04:38:36 +0900

    fixed conv loop, added filters

--- convolution.py
+++ convolution.py
@@ -1,11 +1,11 @@
-from pytools import F
-from getimages import _imshow
-from matrix_multiplication import init_mat, matmul_seq
 from timeit import timeit
+import json
+
 import pyopencl as cl
 import numpy as np
 import torch
-import json
+
+import getimages
 
 SAMPLE_BIN = "./cifar10/sample_bin_data.json"
 IMG = "./cifar10/__DEER_muntjac_s_001000.png"
@@ -33,27 +33,54 @@ def convolution(A,filter):
     #     for j in range(3+2):
     #         print(A[0,i:i+3,j:j+3])
 
-    C = np.zeros_like(A)
-    paddedA = np.pad(A,1)[1:IMGSHAPE[0]+1]
+    C = np.zeros_like(A)#,dtype="float64")
+    paddedA = np.pad(A,1)[1:A.shape[0]+1]
+    # print(paddedA.shape)
     # convolve
     for k in range(paddedA.shape[0]):
-        for i in range(filter.shape[1]+2):
-            for j in range(filter.shape[0]+2):
+        for i in range(A.shape[1]):
+            for j in range(A.shape[2]):
                 # print(A[k,i:i+3,j:j+3])
                 tmpA = paddedA[k,i:i+3,j:j+3]
                 tmpC = np.zeros_like(filter)
                 # print(tmpA.shape, tmpC.shape, filter.shape)
                 # print(tmpA)
                 np.matmul(tmpA,filter,tmpC)
-                C[k,i,j] = tmpC.sum()
+                C[k,i,j] = tmpC.sum()/np.count_nonzero(filter)
+    # print((tmpA @ filter).sum(), tmpC.sum(), np.dot(tmpA,filter).sum())
+
     return C
 
 if __name__=="__main__":
-    IMGSHAPE = (3,5,5)
-    A = init_3darray(IMGSHAPE, np.int64)
-    B = np.array([[1,1,1],[1,1,1],[1,1,1]],
-        dtype=np.int64) # (3,3)
-    C = convolution(A,B)
-    print(C)
-    # C2 = np.zeros_like(A)
-    # print(C2)
+    """input data"""
+    fname, data = getimages.get_images(20)
+    print(data.shape)
+    getimages._imshow(data)
+    # A = init_3darray(data.shape, np.int64)
+    # A.fill(1)
+
+    """filters"""
+    B = np.array([[0,0,1],
+                  [1,0,0],
+                  [0,1,1]], dtype=np.int64) # (3,3)
+    ones = np.ones_like(B)
+    intensifiedSharpen = np.array(
+    [[-1,-1,-1],[-1,9,-1],[-1,-1,-1]], dtype=np.int0) # (3,3)
+    laplacian = np.array((
+    [0, 1, 0],
+    [1, -4, 1],
+    [0, 1, 0]), dtype="int")
+    sharper = np.array((
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]), dtype="int")
+    horiz = np.array((
+    [-1, -2, -1],
+    [0,0,0],
+    [1,2,1]), dtype="int")
+
+    """conv"""
+    FILTER = B
+    C = convolution(data,FILTER)
+    print(C.shape)
+    getimages._imshow(C)
