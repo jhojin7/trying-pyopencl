cf50ec9b3d6cd416a1cbd7f75f162cc6d798baf8
Hojin Jang <jhojin7@gmail.com>
2022-06-12 04:37:23 +0900

    fixed image-related function docs

--- getimages.py
+++ getimages.py
@@ -6,9 +6,12 @@ import PIL.Image as im
 # # download CIFAR10 dataset
 # torchvision.datasets.CIFAR10("./cifar10/",train=True, download=True)
 
-def get_images(INDEX:int):
+def get_images(INDEX:int, dims:tuple = (3,32,32)):
     """Gets images from dataset by index.
-    returns 1D array image data"""
+    returns 1D array image data.
+    - INDEX: i-th image from dataset
+    - dims: image dimensions (k,i,j), default=(3,32,32)
+    """
     # unpickle batch data
     BATCH1 = "./cifar10/cifar-10-batches-py/data_batch_1"
     with open(BATCH1, 'rb') as fo:
@@ -33,31 +36,42 @@ def get_images(INDEX:int):
     IMGDATA = dict[b"data"][INDEX] #3072=32*32*3
     print(label_names[LABEL], FNAME,IMGDATA, sep="\t")
 
-    imgdata = _img_reshape(IMGDATA,(32,32,3),'F')#CFAK
-    # image = _imshow(imgdata,f"{label_names[LABEL]}.jpg")
-    image = _imshow(imgdata,FNAME)
+    imgdata = _img_reshape(IMGDATA,'F',dims)#CFAK
     return (FNAME,imgdata)
 
-# make 2D matrix for img preview
-def _img_reshape(imgdata,shape,order="F"):
+def _img_reshape(imgdata,order="F",shape=(32,32,3)):
+    """make 2D matrix for img preview
+    - imgdata: image data
+    - order="F": order character for reshaping np.array (CFAK)
+    - shape=(32,32,3): destination shape
+    """
     arr = np.array(imgdata).reshape(*shape,order=order)
     # print(arr, arr.size)
     return arr
 
-# show image
-def _imshow(imgdata,fname):
+def _imshow(imgdata,fname=None):
+    """show image.
+    - imgdata: array
+    - fname=None: file name. if None, only show image
+    """
     # https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
     # https://www.codegrepper.com/code-examples/python/convert+3d+array+to+image+python
+    imgdata = _img_reshape(imgdata,'F')#CFAK
     image = im.fromarray(imgdata,"RGB")
-    image.save(fname,"PNG")
-    # image.show()
-    return image
+    if fname != None:
+        image.save(fname,"PNG")
+        return image
+    else:
+        image.show()
 
 if __name__ == "__main__":
-
+############
+    dims = (3,32,32)
+############
     dict = {}
     for i in range(10,20): #set index range
         fname, data = get_images(i)
+        print(data.shape)
         dict[fname] = data.tolist()
     with open("./cifar10/sample_bin_data.json","w") as f:
         import json
